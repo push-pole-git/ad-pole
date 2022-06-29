a6e4b648fcf0b0f9899141d98f4d79d0d1b9adc5
yousef <abdolmalekiyousef@gmail.com>
2022-06-29 17:41:54 +0430

    Call InterstitialAd from user app

--- app/src/main/java/com/example/adpole/MainActivity.java
+++ app/src/main/java/com/example/adpole/MainActivity.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.adpolelib.AdPole;
 import com.example.adpolelib.InAppAdvertise;
+import com.example.adpolelib.Interfaces.AdPoleLoadDataListener;
 import com.example.adpolelib.InterstitialActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AdPole.initialize(this, "faf795df-b9fb-4ecb-8ed4-f20303f73267_CID_99fbb0fb-0d9c-4c0d-b324-9acbb355bdb2");
+        //AdPole.initialize(this, "faf795df-b9fb-4ecb-8ed4-f20303f73267_CID_99fbb0fb-0d9c-4c0d-b324-9acbb355bdb2");
         initInApp();
     }
     private void initInApp() {
-        InAppAdvertise.init(this);
-        InterstitialActivity.show(this, "bb0b964d-268d-4ee6-b5fd-ba7e63684e20",false);
+        InAppAdvertise.init(getApplicationContext());
+        InterstitialActivity.loadAd(getApplicationContext(), "bb0b964d-268d-4ee6-b5fd-ba7e63684e20",false);
+        InterstitialActivity.setAdPoleLoadDataListener(new AdPoleLoadDataListener() {
+            @Override
+            public void onAdLoaded() {
+                if(InterstitialActivity.isLoaded)
+                    InterstitialActivity.show(getApplicationContext());
+            }
+            @Override
+            public void onAdFailedToLoad() {
+                InterstitialActivity.loadAd(getApplicationContext(),"bb0b964d-268d-4ee6-b5fd-ba7e63684e20",false);
+            }
+        });
+
 
     }
 }
